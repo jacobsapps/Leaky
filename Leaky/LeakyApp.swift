772ae0475f63b2f2d962ec68d1cff87e7c6ed24e
Jacob Bartlett <jacob@lapse.com>
2025-01-14 21:09:28 +0000

    Initial Commit

---
+++ Leaky/LeakyApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeakyApp.swift
+//  Leaky
+//
+//  Created by Jacob Bartlett on 14/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct LeakyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
